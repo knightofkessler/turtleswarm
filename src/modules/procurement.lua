fc86162f41e879e69f8964c690baf1b51e21c5f1
Joshua Fitzgerald <joshfitz@vt.edu>
2023-01-03 18:02:35 -0500

    Scaffolding the procurement module

--- src/modules/procurement.lua
+++ src/modules/procurement.lua
@@ -14,12 +14,17 @@ procurement = {}
 
 -----
 -- Attempts to create 
--- @tparam table recipe The recipe to craft, provided in crafting table notation
--- @treturn bool Whether crafting succeeded
+-- @tparam string item A string containing the identifier of the item to be obtained
 function procurement.obtainItemTree(item)
+
+    --General tree-building logic, including finding the relevant procurement type and calling it
+    --to build nongeneric parts of the tree, goes here
 
 end
 
+------------------------------------------------------------------------------------------
+---                            PROCUREMENT TYPES                                       ---
+------------------------------------------------------------------------------------------
 
 ---The subtable for procurement type functions
 -- @table procurementTypes
@@ -30,12 +35,16 @@ procurement.procurementTypes = {}
 -- @tparam itemProfile the itemProfile for the specific item to be procured
 function procurement.procurementTypes.ingot(itemProfile)
 
+    --Tree building logic specific to this procurement type goes here
+
 end
 
 -----
 -- Creates a subtree for procuring mined resources
 -- @tparam itemProfile the itemProfile for the specific item to be procured
 function procurement.procurementTypes.mined(itemProfile)
+
+    --Tree building logic specific to this procurement type goes here
 
 end
 
@@ -44,47 +53,80 @@ end
 -- @tparam itemProfile the itemProfile for the specific item to be procured
 function procurement.procurementTypes.crafted(itemProfile)
 
+    --Tree building logic specific to this procurement type goes here
+
 end
 
----The subtable for item profiles
+-----
+-- Creates a subtree for procuring resources which we don't actively try to get but which 
+-- might be useful if we accidentally do (like iron blocks)
+-- @tparam itemProfile the itemProfile for the specific item to be procured
+function procurement.procurementTypes.incidental(itemProfile)
+    -- I don't think we need any extra code here; for incidental items, we'll just want to 
+    -- look in the turtle's inventory and any chests, as we would for all other items.
+end
+
+------------------------------------------------------------------------------------------
+---                            ITEM PROFILES                                           ---
+------------------------------------------------------------------------------------------
+
+---The submodule for item profiles
 -- @table itemProfiles
 procurement.itemProfiles = {}
 
----An item profile for iron ingots
--- @table itemProfiles.ironIngot
-procurement.itemProfiles.ironIngot = {
+----------------
+---iron stuff---
+----------------
+
+procurement.itemProfiles["minecraft:ironIngot"] = {
     ["procurementType"] = procurement.procurementTypes.ingot,
     ["nuggetType"] = "minecraft:iron_nugget",
     ["blockType"] = "minecraft:iron_block",
     ["oreType"] = "minecraft:iron_ore"
 }
 
----An item profile for iron ore
--- @table itemProfiles.ironOre
-procurement.itemProfiles.ironOre = {
+procurement.itemProfiles["minecraft:iron_nugget"] = {
+    ["procurementType"] = procurement.procurementTypes.incidental,
+}
+
+procurement.itemProfiles["minecraft:iron_block"] = {
+    ["procurementType"] = procurement.procurementTypes.incidental,
+}
+
+procurement.itemProfiles["minecraft:iron_ore"] = {
     ["procurementType"] = procurement.procurementTypes.mined,
     ["miningLevel"] = 9
 }
 
----An item profile for gold ingots
--- @table itemProfiles.goldIngot
-procurement.itemProfiles.goldIngot = {
+----------------
+---gold stuff---
+----------------
+
+procurement.itemProfiles["minecraft:gold_ingot"] = {
     ["procurementType"] = procurement.procurementTypes.ingot,
     ["nuggetType"] = "minecraft:gold_nugget",
     ["blockType"] = "minecraft:gold_block",
     ["oreType"] = "minecraft:gold_ore"
 }
 
----An item profile for gold ore
--- @table itemProfiles.goldOre
-procurement.itemProfiles.goldOre = {
+procurement.itemProfiles["minecraft:gold_nugget"] = {
+    ["procurementType"] = procurement.procurementTypes.incidental,
+}
+
+procurement.itemProfiles["minecraft:gold_block"] = {
+    ["procurementType"] = procurement.procurementTypes.incidental,
+}
+
+procurement.itemProfiles["minecraft:gold_ore"] = {
     ["procurementType"] = procurement.procurementTypes.mined,
     ["miningLevel"] = 9
 }
 
----An item profile for a furnace
--- @table itemProfiles.furnace
-procurement.itemProfiles.furnace = {
+---------------------
+---craftable stuff---
+---------------------
+
+procurement.itemProfiles["minecraft:furnace"] = {
     ["procurementType"] = procurement.procurementTypes.crafted,
     ["recipe"] = {"minecraft:cobblestone","minecraft:cobblestone","minecraft:cobblestone",
                   "minecraft:cobblestone",nil,                    "minecraft:cobblestone",
